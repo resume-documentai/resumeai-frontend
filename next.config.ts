f281faa60b6dedf9746ce055c2852b2ced06fd3d
AshwinKTyagi <ashwin.k.tyagi@gmail.com>
2025-07-06 15:13:32 -0700

    Set up frontend to communicate with backend under new project organization

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       return [
         {
           source: "/api/:path*",
-          destination: "http://backend:8000/:path*"
+          destination: process.env.BACKEND_URL + "/:path*"
         }
       ]
     }
